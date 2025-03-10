5897349385d6c57d403e080e47eeeb00b30ca802
mojya0126 <mojya0126@gmail.com>
2025-03-10 21:54:21 +0900

    section01_23

--- section1.php
+++ section1.php
@@ -9,18 +9,59 @@
 // 'こんにちは'
 // "こんばんは"
 // 'こんば"んは"'
-$array_member = [
-    'name' => '山田',
-    'height' => 180,
-    'hobby' => '野球'
-];
+
+function getChallenge($string){
+    return $string;
+}
+
+
+echo '<br>';
+getChallenge($result);
+$result = '成功';
+
+function test(){
+    echo 'test';
+}
+
+test();
+
+$comment = 'コメント';
+
+function getComment($string){
+    echo $string;
+}
+
+echo '<br>';
+getComment($comment);
+
+
+for($i = 0; $i < 10; $i++){
+    if($i === 5){
+        echo $i;
+    }
+}
 
 $array_fruits = ['appple', 'banana', 'cherry'];
 
+echo '<br>';
 var_dump($array_fruits);
 
+$array_members = [
+    [
+    'name' => '山田',
+    'height' => 180,
+    'hobby' => '野球'
+    ],
+    [
+        'name' => '佐藤',
+        'height' => 170,
+        'hobby' => '野球'
+    ]
+];
+
+echo '<br>';
 foreach($array_members as $array_member){
-    echo $array_member['name'];
+    echo $array_member['name'] . "\n";
 }
 
-var_dump($test);
+// var_dump($test);
